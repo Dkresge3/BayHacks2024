d5aca6b7aaa2a1b0b77df94ce70a7a4e59e669d3
EvagelosPetropoulos <vagelispetropoulos@yahoo.com>
2024-02-25 13:53:03 -0500

    text change to dog component

--- smart-collar/src/components/Dog.js
+++ smart-collar/src/components/Dog.js
@@ -3,9 +3,13 @@ import './Dog.css'
 
 function Dog() {
   return (
-    <button onClick={() => (window.location.href = "http://192.168.101.225:8080/")}>
-       Calculate
-</button>
+    <p>
+        Check on your dogs and see how long they have gone without a walk
+        <br></br>
+        <button onClick={() => (window.location.href = "http://192.168.101.225:8080/")}>
+        Calculate
+        </button>
+    </p>
   )
 }
 
